4ee868ecceb5544be010b53e4310464de14a32b0
Peremil <peremilsoderstrom2@hotmail.com>
2025-05-09 15:31:35 +0200

    trying out flask stuff + html

---
+++ log_in_info.py
@@ -0,0 +1,4 @@
+user_info = [{"id": "matt", "pw": "hello"},
+             {"id": "sam", "pw": "bye"},
+             {"id": "margit", "pw": "tarnished"}]
+
--- web_app.py
+++ web_app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
-
+from log_in_info import user_info
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+    return render_template("index.html", signed = user_info)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
