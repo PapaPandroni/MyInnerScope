6a3f4d937acb6dfe3562794629e864d8479731a1
Peremil <peremilsoderstrom2@hotmail.com>
2025-05-10 11:51:34 +0200

    started on the real pagelayout

--- web_app.py
+++ web_app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
-from log_in_info import user_info
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("index.html", signed = user_info)
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
