927a4c5e024d05faf52fad757c40f57cebb89022
Peremil <peremilsoderstrom2@hotmail.com>
2025-05-18 20:50:22 +0200

    added skeleton for progress page

--- web_app.py
+++ web_app.py
@@ -76,6 +76,10 @@ def register():
 
     return render_template("register.html")
 
+@app.route("/progress")
+def progress():
+    return render_template("progress.html")
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
