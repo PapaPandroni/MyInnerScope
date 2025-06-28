2f2a562aa3bde5823a1ef1ebd697335fd0d93395
Peremil <peremilsoderstrom2@hotmail.com>
2025-06-28 22:15:56 +0200

    added custom error 404 403 and 500 pages

--- web_app.py
+++ web_app.py
@@ -48,6 +48,18 @@ def create_app(config_name=None):
     def hello():
         return render_template("index.html")
     
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('errors/404.html'), 404
+
+    @app.errorhandler(403)
+    def forbidden(e):
+        return render_template('errors/403.html'), 403
+
+    @app.errorhandler(500)
+    def internal_server_error(e):
+        return render_template('errors/500.html'), 500
+
     return app
 
 if __name__ == "__main__":
