f5af484a67144552f3a52a4ceea8827a6dfc6399
Peremil <peremilsoderstrom2@hotmail.com>
2025-07-06 19:18:57 +0200

    cleaned up my git tracking

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .database import db
-from .user import User
-from .diary_entry import DiaryEntry
-from .daily_stats import DailyStats
-from .goal import Goal
-
-__all__ = ['db', 'User', 'DiaryEntry', 'DailyStats', 'Goal']
--- utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .search_helpers import handle_search, create_search_snippet
-
-__all__ = ['handle_search', 'create_search_snippet']
